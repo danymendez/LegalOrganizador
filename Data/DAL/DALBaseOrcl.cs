4ff88cf202b85377ebdd46ff6a03ce14fee98e77
Daniel Méndez Palacios <dmendezp@outlook.com>
2018-10-24 17:00:21 -0600

    Mejoras en ORM

--- Data/DAL/DALBaseOrcl.cs
+++ Data/DAL/DALBaseOrcl.cs
@@ -67,7 +67,28 @@ namespace Data.DAL
                 command.Parameters.Add(param.ParameterName, param.Value);
             }
 
-            command.ExecuteNonQuery();
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception if the transaction fails to commit.
+                Console.WriteLine(ex.Message);
+
+                try
+                {
+                    // Attempt to roll back the transaction.
+                    sqlTran.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    // Throws an InvalidOperationException if the connection 
+                    // is closed or the transaction has already been rolled 
+                    // back on the server.
+                    Console.WriteLine(exRollback.Message);
+                }
+            }
             return entity;
         }
 
