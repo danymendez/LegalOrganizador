3decaf1f189e538bf61bae8ec0260e2841b4eea8
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-02-16 23:01:50 -0600

    LAST VERSION 16022019

--- PreOrclFrontEnd/Helpers/GraphSdkHelper.cs
+++ PreOrclFrontEnd/Helpers/GraphSdkHelper.cs
@@ -20,19 +20,20 @@ namespace PreOrclFrontEnd.Helpers
         // Get an authenticated Microsoft Graph Service client.
         public GraphServiceClient GetAuthenticatedClient(string userId)
         {
+            string c = "";
             _graphClient = new GraphServiceClient(new DelegateAuthenticationProvider(
                 async requestMessage =>
                 {
                     // Passing tenant ID to the sample auth provider to use as a cache key
                     var accessToken = await _authProvider.GetUserAccessTokenAsync(userId);
-
+                
                     // Append the access token to the request
                     requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
                     // This header identifies the sample in the Microsoft Graph service. If extracting this code for your project please remove.
                     requestMessage.Headers.Add("SampleID", "aspnetcore-connect-sample");
                 }));
-
+            c = _authProvider.GetUserAccessTokenAsync(userId).Result;
             return _graphClient;
         }
     }
