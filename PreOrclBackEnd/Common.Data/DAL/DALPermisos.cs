c7e41eca9ead6b92889138af308d0e4106461d54
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-03-18 12:07:29 -0600

    DALPermisos Añadidos

--- PreOrclBackEnd/Common.Data/DAL/DALPermisos.cs
+++ PreOrclBackEnd/Common.Data/DAL/DALPermisos.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using Common.Entity.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Common.Data.DAL
 {
-    class DALPermisos
+    public class DALPermisos : DALBaseOrcl
     {
+
+        private DALDBContext context;
+        public DALPermisos(DALDBContext context)
+            : base(context)
+        {
+            this.context = context;
+
+        }
+
+
+        public Permisos CreatePermisos(Permisos permisos)
+        {
+
+            return Create(permisos);
+
+        }
+
+        public List<Permisos> GetAllPermisos()
+        {
+            return GetAll<Permisos>();
+        }
+
+        public Permisos DeleteRol(int id)
+        {
+            return Delete<Permisos>(id);
+        }
+
+        public Permisos UpdatePermisos(long id, Permisos permisos)
+        {
+
+            return Update(id, permisos);
+        }
+
+        public Permisos GetPermisos(long id)
+        {
+            return Get<Permisos>(id);
+        }
     }
 }
