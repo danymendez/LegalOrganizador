9c466ae845aba0405a2509aca60beadaaeb579ae
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-04-02 16:18:55 -0600

    Se agregan nuevo modulos

---
+++ PreOrclFrontEnd/Models/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreOrclFrontEnd.Models
+{
+    public class Roles
+    {
+        public double IdRol { get; set; }
+    }
+}
