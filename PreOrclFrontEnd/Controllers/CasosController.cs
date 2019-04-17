fa1ebbd8a28ad811893ab79bc483e6872fb85505
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-04-17 14:45:11 -0600

    Mejoras en css

---
+++ PreOrclFrontEnd/Controllers/CasosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Options;
+using PreOrclFrontEnd.Helpers;
+using PreOrclFrontEnd.Models;
+using PreOrclFrontEnd.Utilidades;
+
+namespace PreOrclFrontEnd.Controllers
+{
+    public class CasosController : Controller
+    {
+
+        GenericREST generic;
+
+        public CasosController(IOptions<UriHelpers> configuration)
+        {
+
+            generic = new GenericREST(configuration.Value);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+     
+        public IActionResult Create()
+        {
+            ViewBag.listaPersonas = new SelectList(generic.GetAll<SisPerPersona>("SisPerPersonas").Result, "per_IDPER", "per_nombre_razon"); 
+            return View();
+        }
+
+    }
+}
---
+++ PreOrclFrontEnd/Models/Casos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreOrclFrontEnd.Models
+{
+    public class Casos
+    {
+       
+        public decimal IdCaso { get; set; }
+        [Required]
+        public DateTime FechaApertura { get; set; }
+      
+        [Required]
+        public decimal IdCliente { get; set; }
+
+        [MaxLength(1)]
+        [Required]
+        public string Categoria { get; set; }
+
+        [MaxLength(1)]
+        [Required]
+        public string Tipo { get; set; }
+
+        [Required]
+        public decimal PrecioPactado { get; set; }
+
+        [Required]
+        public decimal IdAbogado { get; set; }
+
+        [Required]
+        public decimal IdCreador { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? InactivatedAt { get; set; }
+
+        [Required]
+        public decimal Inactivo { get; set; }
+    }
+}
