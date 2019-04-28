452ef5dee6149d7cb7cdc25e5d33fe3de4eeec50
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-04-28 09:17:54 -0600

    se crea lista de usuarios

--- PreOrclFrontEnd/Controllers/UsuariosController.cs
+++ PreOrclFrontEnd/Controllers/UsuariosController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PreOrclFrontEnd.Helpers;
+using PreOrclFrontEnd.Models;
+using PreOrclFrontEnd.Utilidades;
 
 namespace PreOrclFrontEnd.Controllers
 {
     public class UsuariosController : Controller
     {
-        public IActionResult Index()
+        GenericREST generic;
+
+        public UsuariosController(IOptions<UriHelpers> configuration)
         {
-            return View();
+
+            generic = new GenericREST(configuration.Value);
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Usuarios> listaUsuarios = await generic.GetAll<Usuarios>("Usuarios");
+
+            ViewBag.PersonasClassCssNav = "active";
+            return View(listaUsuarios);
         }
     }
 }
