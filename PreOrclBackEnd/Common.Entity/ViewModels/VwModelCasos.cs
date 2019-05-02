d98ac5279bf0ed9513dd74f26f350b179ebb208d
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-05-01 23:08:56 -0600

    Se suben mejoras en modalview

--- PreOrclBackEnd/Common.Entity/ViewModels/VwModelCasos.cs
+++ PreOrclBackEnd/Common.Entity/ViewModels/VwModelCasos.cs
@@ -10,6 +10,11 @@ namespace Common.Entity.ViewModels
         public Casos Casos { get; set; }
         public SisPerPersona Cliente { get; set; }
 
+        public List<SisPerPersona> ListadoImputados { get; set; }
+
+        public decimal[] IdImputados { get; set; }
+        public List<Documentos> ListadoDocumentos { get; set; }
+        public decimal[] IdDocumentos { get; set; }
         public Usuarios Abogado { get; set; }
         public List<VwModelActividadesAsistentes> ListVwModelActividadesAsistentes { get; set; }
     }
