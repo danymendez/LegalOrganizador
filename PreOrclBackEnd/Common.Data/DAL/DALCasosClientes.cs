b3cd6501d68b72692440a8c84e5d017329ee0986
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-05-01 22:29:32 -0600

    Dal Casos clientes

---
+++ PreOrclBackEnd/Common.Data/DAL/DALCasosClientes.cs
@@ -0,0 +1,47 @@
+﻿using Common.Entity.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Data.DAL
+{
+   public class DALCasosClientes : DALBaseOrcl
+    {
+        private DALDBContext context;
+        public DALCasosClientes(DALDBContext context)
+            : base(context)
+        {
+            this.context = context;
+
+        }
+
+
+        public CasosClientes CreateCasosClientes(CasosClientes casosClientes)
+        {
+
+            return Create(casosClientes);
+
+        }
+
+        public List<CasosClientes> GetAllCasosClientes()
+        {
+            return GetAll<CasosClientes>();
+        }
+
+        public CasosClientes DeleteDocumento(decimal id)
+        {
+            return Delete<CasosClientes>(id);
+        }
+
+        public CasosClientes UpdateCasosClientes(decimal id, CasosClientes casosClientes)
+        {
+
+            return Update(id, casosClientes);
+        }
+
+        public CasosClientes GetCasoCliente(decimal id)
+        {
+            return Get<CasosClientes>(id);
+        }
+    }
+}
