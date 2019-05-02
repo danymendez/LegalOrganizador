69f8f8bb73282cbe3f46af51c6da1a173e227d71
Daniel Méndez Palacios <dmendezp@outlook.com>
2019-05-02 17:05:05 -0600

    Se crea modelo de documentos y se modifica viewModel de casos

---
+++ PreOrclFrontEnd/Models/Documentos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreOrclFrontEnd.Models
+{
+    public class Documentos
+    {
+     
+        public decimal IdDocumento { get; set; }
+  
+        public decimal IdCaso { get; set; }
+     
+        public string Url { get; set; }
+        
+        public string Nombre { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+      
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
--- PreOrclFrontEnd/ViewModels/VwModelCasos.cs
+++ PreOrclFrontEnd/ViewModels/VwModelCasos.cs
@@ -8,9 +8,15 @@ namespace PreOrclFrontEnd.ViewModels
 {
     public class VwModelCasos
     {
+
         public Casos Casos { get; set; }
         public SisPerPersona Cliente { get; set; }
 
+        public List<SisPerPersona> ListadoImputados { get; set; }
+
+        public decimal[] IdImputados { get; set; }
+        public List<Documentos> ListadoDocumentos { get; set; }
+        public decimal[] IdDocumentos { get; set; }
         public Usuarios Abogado { get; set; }
         public List<VwModelActividadesAsistentes> ListVwModelActividadesAsistentes { get; set; }
     }
